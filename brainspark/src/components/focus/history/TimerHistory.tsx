b06eb0d66551f10ad35adeb589028aa0848f515b
amadeu100401 <amadeu.martim@souunit.com.br>
2025-05-31 23:36:13 -0300

    feature/custom-scrollbar

--- brainspark/src/components/focus/history/TimerHistory.tsx
+++ brainspark/src/components/focus/history/TimerHistory.tsx
@@ -8,7 +8,7 @@ interface TimerHistoryProps {
 
 export default function TimerHistory({ focusHistory }: TimerHistoryProps) {
   return(
-    <div className="bg-white rounded-lg shadow-sm p-0 max-h-[550px] pl-6 pr-6 pb-6 overflow-y-auto relative">
+    <div className="bg-white rounded-lg shadow-sm p-0 max-h-[550px] pl-6 pr-6 pb-6 overflow-y-auto relative scrollbar-custom">
 
         <div className="sticky top-0 bg-white p-2 z-10 h-30">
             <h2 className="text-lg font-semibold">Registros de tempo</h2>
